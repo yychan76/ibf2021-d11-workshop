62b895cd319c373826ccec221a2269f8c7e46259
Chan Yew Yen <chan.yewyen@gmail.com>
2022-01-05 14:46:30 +0800

    Initial commit

---
+++ src/test/java/nus/edu/sg/workshop11/Workshop11ApplicationTests.java
@@ -0,0 +1,13 @@
+package nus.edu.sg.workshop11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
